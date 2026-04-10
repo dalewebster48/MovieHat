edeaa834738f2a55e28517de1e4ea18b6cae4d33
Dale Webster <dalewebster48@gmail.com>
2026-04-10 23:22:29 +0100

    Initial Commit

---
+++ MovieHat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieHat
+//
+//  Created by Dale Webster on 10/04/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
